7464b10ad1901feb9b4553bace10f4227e9e0f06
Leiner Ortega <leicortega@gmail.com>
2019-11-13 09:50:31 -0500

    Se arreglo la consulta para agregar los productos

--- admin/agregar_producto/validaciones/agregar-img.php
+++ admin/agregar_producto/validaciones/agregar-img.php
@@ -35,23 +35,29 @@ if (move_uploaded_file($_FILES['imagen']['tmp_name'], $guardar_imagen)) {
 	// echo "<br>".$nombre_imagen;
 
 	$sql = $conexion->prepare("INSERT INTO `producto_editorial` (`id_producto`, `nombre_producto`, `descripcion_producto`, `precio_producto`, `autor_producto`, `editorial_producto`, `edicion_producto`, `formato_producto`, `ISBN_producto`, `facultad`, `n_paginas`, `idioma`, `terminado`, `alto_ancho`, `imagen`, `id_categoria_fk`) VALUES (NULL, :nombre_producto, :descripcion_producto, :precio_producto, :autor_producto, :editorial_producto, :edicion_producto, :formato_producto, :ISBN_producto, :facultad, :n_paginas, :idioma, :terminado, :alto_ancho, :imagen, 1)");
-	$sql->bindParam(":nombre_producto", $_POST['nombre_producto']);
-	$sql->bindParam(":descripcion_producto", $_POST['descripcion_producto']);
-	$sql->bindParam(":precio_producto", $_POST['precio_producto']);
-	$sql->bindParam(":autor_producto", $_POST['autor_producto']);
-	$sql->bindParam(":editorial_producto", $_POST['editorial_producto']);
-	$sql->bindParam(":edicion_producto", $_POST['edicion_producto']);
-	$sql->bindParam(":formato_producto", $_POST['formato_producto']);
-	$sql->bindParam(":ISBN_producto", $_POST['ISBN_producto']);
-	$sql->bindParam(":facultad", $_POST['facultad']);
-	$sql->bindParam(":n_paginas", $_POST['n_paginas']);
-	$sql->bindParam(":idioma", $_POST['idioma']);
-	$sql->bindParam(":terminado", $_POST['terminado']);
-	$sql->bindParam(":alto_ancho", $_POST['alto_ancho']);
-	$sql->bindParam(":imagen", $nombre_imagen);
+	$sql->bindParam(":nombre_producto", $_POST['nombre_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":descripcion_producto", $_POST['descripcion_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":precio_producto", $_POST['precio_producto'], PDO::PARAM_INT);
+	$sql->bindParam(":autor_producto", $_POST['autor_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":editorial_producto", $_POST['editorial_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":edicion_producto", $_POST['edicion_producto'], PDO::PARAM_INT);
+	$sql->bindParam(":formato_producto", $_POST['formato_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":ISBN_producto", $_POST['ISBN_producto'], PDO::PARAM_STR);
+	$sql->bindParam(":facultad", $_POST['facultad'], PDO::PARAM_STR);
+	$sql->bindParam(":n_paginas", $_POST['n_paginas'], PDO::PARAM_INT);
+	$sql->bindParam(":idioma", $_POST['idioma'], PDO::PARAM_STR);
+	$sql->bindParam(":terminado", $_POST['terminado'], PDO::PARAM_STR);
+	$sql->bindParam(":alto_ancho", $_POST['alto_ancho'], PDO::PARAM_STR);
+	$sql->bindParam(":imagen", $nombre_imagen, PDO::PARAM_STR);
 	$sql->execute();
+	if ($sql->rowCount() == 1) {
+		header("location:../");
+	} else {
+		print_r( $sql->errorInfo() );
+	}
+	
 	// echo $sql->rowCount();
-	header("location:../");
+	
 
 } else {
 	echo 0;
